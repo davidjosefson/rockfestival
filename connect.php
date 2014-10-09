ff56598d0cbfea79242a9136e2cc45e6c965011f
David Josefson <david.josefson@gmail.com>
2014-10-09 19:17:08 +0200

    Removed unnecessary code and comments, and it should be as up to date as possible now.

--- connect.php
+++ connect.php
@@ -4,19 +4,6 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'root'); 
 define('DB_HOST', 'localhost');
 
-/*
-$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-
-mysql_set_charset('utf8',$link);
-
-if(!$link)
-    die('Kunde inte konnekta! Error: ' . mysql_error());
-
-$db_selected = mysql_select_db(DB_NAME, $link);
-
-if(!$db_selected)
-        die('Kunde inte konnekta till databas ' . DB_NAME . " : " . mysql_error());*/
-
 //PDO
 try {
     $DBH = new PDO("mysql:host=".DB_HOST."; dbname=".DB_NAME."; charset=utf8mb4", DB_USER, DB_PASSWORD);
@@ -26,8 +13,6 @@ try {
 catch (PDOException $e) {
     echo "Ajajdå.. ", $e->getMessage();
 }
-
-//$STH = $DBH->query('SELECT * FROM Funktionar');    
 
 header('Content-Type: text/html; charset=UTF-8');
 setlocale(LC_ALL, 'sv_SE.UTF-8', 'sve');  
