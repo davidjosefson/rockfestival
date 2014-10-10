6f7ad5a4ed2bbc604e8a417a13f0cc30d574a3f7
David Josefson <david.josefson@gmail.com>
2014-10-10 22:56:25 +0200

    Readded the navbar to be able to choose whether or not to show it (useful on the startpage where we want to hide it)

---
+++ navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+            <a class="navbar-brand" href="index.php">Rockfestival.</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Lägg till <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="funktionar.php">Funktionär</a></li>
+                    <li><a href="#">Säkerhetspass</a></li>
+                    <li><a href="scen.php">Scen</a></li>
+                    <li class="divider"></li>
+                    <li><a href="AddBand.php">Band</a></li>
+                    <li><a href="bandmedlem.php">Bandmedlem</a></li>
+                    <li><a href="spelning.php">Spelning</a></li>
+                  </ul> 
+                </li>
+                <li><a href="#">Band</a></li>
+                <li><a href="schema.php">Program</a></li>
+                <li><a href="sakerhetslista.php">Säkerhetslista</a></li>
+                <li><a href="kontaktpersoner.php">Kontaktpersoner</a></li>
+            </ul>                                         
+        </div>
+    </div>
+</nav>
