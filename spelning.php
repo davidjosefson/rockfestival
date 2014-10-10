110bd65f80e94870a120f8e521efdd6e5f8adb68
David Josefson <david.josefson@gmail.com>
2014-10-10 16:28:15 +0200

    Added dropdownboxes with correct options and a table for previously added concerts at the bottom

--- spelning.php
+++ spelning.php
@@ -39,9 +39,8 @@ ini_set('display_startup_errors',1);
     $STHscen = $DBH->query('SELECT Namn FROM Scen');
     $STHday = $DBH->query('SELECT Dag FROM Festivaldag');
     $STHtime = $DBH->query('SELECT Tid FROM Scentid');
-    $STHbandTable = $DBH->query('SELECT Band.Namn, Landskod, Grundades, Musikstil, Trivia, Funktionar.Namn as Kontaktperson
-                        FROM Band LEFT JOIN Funktionar
-                        ON Band.Kontaktperson = Funktionar.FunktionarsID');    
+    $STHspelningTable = $DBH->query('SELECT Namn Band, Scen, Festivaldag, Scentid, Starttid
+    FROM Band INNER JOIN Spelning ON Band.BandID = Spelning.Band ORDER BY Starttid asc;');    
 
     //Fill the arrays with data
     while($row = $STHbandName->fetch()) { 
@@ -51,63 +50,61 @@ ini_set('display_startup_errors',1);
         $scenArray[] = $row;
     }    
     while($row = $STHday->fetch()) { 
-        $dayArray[] = $row;
+        $dagArray[] = $row;
     }    
     while($row = $STHtime->fetch()) { 
-        $timeArray[] = $row;
+        $tidArray[] = $row;
     }    
-    while($row = $STHbandTable->fetch()) {
-        $bandTable[] = $row;
+    while($row = $STHspelningTable->fetch()) {
+        $spelningTable[] = $row;
     }
 ?>
 
 <?php include('header.php'); ?>
        <div class="col-md-4">
         <div class="page-header">
-            <h2>Lägg till band</h2>
+            <h2>Lägg till spelning</h2>
         </div>
 
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
               enctype="application/x-www-form-urlencoded" role="form" />
             <div class="form-group">
                 <label class="control-label" for="band">Band:</label>
-                <select name="band" id="band">  
+                <select name="band" id="band" class="form-control">  
                     <?php foreach($bandNameArray as $band) : ?>
-                    <option value="<?php echo $band['BandID'] ?>"><?php $band['Namn'] ?></option>
+                    <option value="<?php echo $band['BandID']; ?>"><?php echo $band['Namn']; ?></option>
                     <?php endforeach; ?>
-                </select>>
-
-            </div>
-            <div class="form-group">
-                <label class="control-label" for="landskod">Landskod:</label>
-                <input type="text" name="landskod" pattern="[A-Za-z]{3}"  class="input-large form-control"
-                       id="landskod" placeholder="Ex: SWE" title="Landskod i form av 3 bokstäver, t.ex SWE" required/>
-            </div>
-            <div class="form-group">
-                <label class="control-label" for="grundades">Grundades:</label>
-                <input type="number" name="grundades" min="1800" max="2014" class="form-control" id="grundades"
-                       placeholder="Ex. 1987" title="Årtal mellan 1800-2014" required/>
-            </div>
-            <div class="form-group">
-                <label class="control-label" for="musikstil">Musikstil:</label>
-                <input type="text" name="musikstil" maxlength="50" class="input-large form-control"
-                       id="musikstil" placeholder="Genre" title="Max 50 tecken" required/>
-            </div>
-           
-             <div class="form-group">
-                 <label class="control-label" for="trivia" >Trivia:</label>
-                 <textarea class="form-control" rows="3" name="trivia" id="trivia" required></textarea>
-            </div>
-            <div class="form-group">
-                <label class="control-label" for="funktionar" >Kontaktperson:</label>
-                <select name="funktionar" id="funktionar">  
-                    <?php
-                        $STHtest = $DBH->query('SELECT FunktionarsID, Namn FROM Funktionar');   
-                        while($row = $STHtest->fetch()) { 
-                            echo '<option value="' . $row['FunktionarsID'] . '"> ' . $row['Namn'] .     '</option>';  
-                        }
-                    ?>  
                 </select>
+
+           </div>
+           <div class="form-group">
+                <label class="control-label" for="scen">Scen:</label>
+                <select name="scen" id="scen" class="form-control">  
+                    <?php foreach($scenArray as $scen) : ?>
+                    <option value="<?php echo $scen['Namn']; ?>"><?php echo $scen['Namn']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+           </div>
+           <div class="form-group">
+                <label class="control-label" for="dag">Dag:</label>
+                <select name="dag" id="dag" class="form-control">  
+                    <?php foreach($dagArray as $dag) : ?>
+                    <option value="<?php echo $dag['Dag']; ?>"><?php echo $dag['Dag']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+           </div>
+           <div class="form-group">
+                <label class="control-label" for="tid">Tid:</label>
+                <select name="tid" id="tid" class="form-control">  
+                    <?php foreach($tidArray as $tid) : ?>
+                    <option value="<?php echo $tid['Tid']; ?>"><?php echo $tid['Tid']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+           </div>
+            <div class="form-group">
+                <label class="control-label" for="starttid">Starttid:</label>
+                <input type="text" name="starttid" pattern="[A-Za-z]{3}"  class="input-large form-control"
+                       id="starttid" placeholder="Ex: 16:30" title="Klockslag, t.ex 16:30" required/>
             </div>
 
             <input type="submit" value="Skicka" class="btn btn-default" name="insert"/>
@@ -120,23 +117,23 @@ ini_set('display_startup_errors',1);
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th>Namn</th>
-                    <th>Landskod</th>
-                    <th>Grundades</th>
-                    <th>Musikstil</th>
-                    <th>Trivia</th>
-                    <th>Kontaktperson</th>
+                    <th>Band</th>
+                    <th>Scen</th>
+                    <th>Festivaldag</th>
+                    <th>Scentid</th>
+                    <th>Starttid</th>
                 </tr>
             </thead>
         <tbody>
-        <?php foreach ($bandTable as $row) : ?>
+        <?php foreach ($spelningTable as $row) : ?>
             <tr>
-                <td><?php echo $row['Namn']; ?></td>
-                <td><?php echo $row['Landskod']; ?></td>
-                <td><?php echo $row['Grundades']; ?></td>
-                <td><?php echo $row['Musikstil']; ?></td>
-                <td><?php echo $row['Trivia']; ?></td>
-                <td><?php echo $row['Kontaktperson']; ?></td>
+                <td><?php echo $row['Band']; ?></td>
+                <td><?php echo $row['Scen']; ?></td>
+                <td><?php echo $row['Festivaldag']; ?></td>
+                <td><?php echo $row['Scentid']; ?></td>
+                <td><?php echo $row['Starttid']; ?></td>
+                
+                
             </tr>
          <?php endforeach; ?>   
         </tbody>
