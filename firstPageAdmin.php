05597d58d71d9d36369839231b6d905a9b00b1d1
David Josefson <david.josefson@gmail.com>
2014-10-11 00:17:59 +0200

    Added different first-pages depending on which type of user you choose to log in as

---
+++ firstPageAdmin.php
@@ -0,0 +1,9 @@
+
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+
+    <div class="page-header">
+        <h2>Välkommen administratör.</h2>
+    </div>
+        
+<?php include('footer.php'); ?>
---
+++ firstPageUser.php
@@ -0,0 +1,8 @@
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+
+    <div class="page-header">
+        <h2>Välkommen festivalbesökare.</h2>
+    </div>
+        
+<?php include('footer.php'); ?>
