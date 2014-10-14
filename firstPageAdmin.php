0259adb0525026d02382bbf2a240ad0f8b69886a
David Josefson <david.josefson@gmail.com>
2014-10-14 13:50:47 +0200

    Added some text on admin first page and some css for adding vertical white space

--- firstPageAdmin.php
+++ firstPageAdmin.php
@@ -5,5 +5,19 @@
     <div class="page-header">
         <h1>Välkommen administratör.</h1>
     </div>
-        
+    
+    <div class="col-md-6">
+        <p>Här kan du se allt som de vanliga festivalbesökarna kan se - plus att du kan lägga till ny information.</p>
+        <p>Vi litar på att du inte missbrukar makten du har givits till att utföra onda handlingar.</p>
+        <!-- <div class="well well-lg"><p class="lead"><em>"With great power comes great responsibility"</em></p></div> -->
+        <blockquote class="vertical-offset-5">
+            <p class="lead">
+                <em>"With great power comes great responsibility"</em>
+            </p>
+            <footer>
+                Spiderman
+            </footer>
+        </blockquote>
+    </div>
+
 <?php include('footer.php'); ?>
